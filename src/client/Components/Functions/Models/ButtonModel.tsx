5e6bc2f884765772995dee36167e83ffa9e47518
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-25 00:51:45 +0900

    update : button model

--- src/client/Components/Functions/Models/ButtonModel.tsx
+++ src/client/Components/Functions/Models/ButtonModel.tsx
@@ -1,11 +1,13 @@
 import * as solid from "solid-js";
 
+import * as playAudio from "Utils/PlayAudio/playButtonAudio";
+
 declare module "solid-js" {
     namespace JSX {
         interface Directives {
             buttonModel: {
-                handleClick: EventListenerOrEventListenerObject,
-                handleHover: EventListenerOrEventListenerObject
+                handleClick?: EventListenerOrEventListenerObject,
+                handleHover?: EventListenerOrEventListenerObject
             };
         }
     }
@@ -13,14 +15,20 @@ declare module "solid-js" {
 
 function buttonModel(el: Element, props: () => solid.JSX.Directives["buttonModel"]) {
     solid.onMount(() => {
-        el.addEventListener("click", props().handleClick);
-        el.addEventListener("pointerenter", props().handleHover);
-        el.addEventListener("focusin", props().handleClick);
+        el.addEventListener("click", props().handleClick || function () { });
+        el.addEventListener("click", playAudio.playClickAudio);
+        el.addEventListener("pointerenter", props().handleHover || function () { });
+        el.addEventListener("pointerenter", playAudio.playHoverAudio);
+        el.addEventListener("focusin", props().handleClick || function () { });
+        el.addEventListener("focusin", playAudio.playHoverAudio);
     });
     solid.onCleanup(() => {
-        el.removeEventListener("click", props().handleClick);
-        el.removeEventListener("pointerenter", props().handleHover);
-        el.removeEventListener("focusin", props().handleClick);
+        el.removeEventListener("click", props().handleClick || function () { });
+        el.removeEventListener("click", playAudio.playClickAudio);
+        el.removeEventListener("pointerenter", props().handleHover || function () { });
+        el.removeEventListener("pointerenter", playAudio.playHoverAudio);
+        el.removeEventListener("focusin", props().handleClick || function () { });
+        el.removeEventListener("focusin", playAudio.playHoverAudio);
     })
 }
 
