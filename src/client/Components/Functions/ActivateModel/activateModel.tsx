1ec9958e5fd7e65b97997f6cbcfd20e01b0f63ce
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-25 00:49:44 +0900

    update : button model

--- src/client/Components/Functions/ActivateModel/activateModel.tsx
+++ src/client/Components/Functions/ActivateModel/activateModel.tsx
@@ -1,5 +1,12 @@
+import buttonModel from "../Models/ButtonModel";
+
 function activateModel(model: any) {
     return model;
 }
 
+function useButtonModel() {
+    return buttonModel;
+}
+
 export default activateModel;
+export { useButtonModel };
