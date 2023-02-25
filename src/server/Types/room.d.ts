60f57f9f34c626144dbb989cea2f7ad9a6bd08f6
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-25 15:37:43 +0900

    remove : server

--- src/server/Types/room.d.ts
+++
@@ -1,7 +0,0 @@
-interface multiPlayerRoom {
-    name: string
-    id: string
-    invite: string,
-    users: Array<wsUser>
-    owner: wsUser
-}
--- src/server/Types/update.d.ts
+++
@@ -1,9 +0,0 @@
-type refType = { [key: string]: { url: string, size: number, hash: string } }
-interface keys {
-    name: string,
-    path: string
-    ext: string
-    url: string
-    ref: refType
-}
-type keyType = Array<keys>
