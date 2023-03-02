0377f69233107f9c721e885cea2fb51cfb14454c
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-03-02 17:53:58 +0900

    update : add return value type

--- src/client/Utils/getConfig/getConfig.ts
+++ src/client/Utils/getConfig/getConfig.ts
@@ -1,24 +1,28 @@
-import { DBVersion, environment, gameConfig, musicSelect, userConfig } from "Utils/Storage/LocalStorage/defaultValue";
+import { DBVersion, environment, gameConfig, musicSelect, resources, userConfig } from "Utils/Storage/LocalStorage/defaultValue";
 
-function getDBVersion() {
+function getDBVersion(): typeof DBVersion {
     return JSON.parse(localStorage.getItem("DBVersion") || JSON.stringify(DBVersion));
 }
 
 
-function getEnvironment() {
+function getEnvironment(): environment {
     return JSON.parse(localStorage.getItem("environment") || JSON.stringify(environment));
 }
 
-function getGameConfig() {
+function getGameConfig(): gameConfig {
     return JSON.parse(localStorage.getItem("gameConfig") || JSON.stringify(gameConfig));
 }
 
-function getUserData() {
+function getUserData(): userConfig {
     return JSON.parse(localStorage.getItem("userData") || JSON.stringify(userConfig));
 }
 
-function getSelectedMusic() {
+function getSelectedMusic(): typeof musicSelect {
     return JSON.parse(localStorage.getItem("musicSelect") || JSON.stringify(musicSelect));
 }
 
-export { getDBVersion, getEnvironment, getGameConfig, getSelectedMusic, getUserData };
+function getResources(): resources {
+    return JSON.parse(localStorage.getItem("resources") || JSON.stringify(resources));
+}
+
+export { getDBVersion, getEnvironment, getGameConfig, getSelectedMusic, getUserData, getResources };
