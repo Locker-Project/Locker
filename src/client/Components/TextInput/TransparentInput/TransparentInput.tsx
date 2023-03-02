69a22cb47779b7039043058b0a38eb5398f51738
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-03-02 20:20:36 +0900

    add : translate input

---
+++ src/client/Components/TextInput/TransparentInput/TransparentInput.tsx
@@ -0,0 +1,28 @@
+import * as solid from "solid-js";
+import clsx from "clsx";
+
+
+import style from "./TransparentInput.module.scss";
+import { useInputModel } from "Components/Functions/ActivateModel/activateModel";
+
+
+const TransparentInput: solid.Component<solid.JSX.InputHTMLAttributes<HTMLInputElement>> = (props) => {
+    const lengthFlag = props.maxLength;
+    const [length, setLength] = solid.createSignal(0);
+
+    const inputModel = useInputModel();
+
+    return (
+        <div class={style.inputContainer}>
+            <input {...props} class={clsx(style.input, props.class)} placeholder={""} use:inputModel={[length, setLength]} />
+            <span class={clsx(style.placeholder, !length() && style.active)}>
+                {props.placeholder}
+            </span>
+            <span class={clsx(style.textCount, lengthFlag && style.active)}>
+                {length} / {props.maxLength}
+            </span>
+        </div>
+    )
+}
+
+export default TransparentInput;
