9077fcb1aeaef1f28ada41a899bd090d83b9b038
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-03-05 13:34:38 +0900

    update : intl

--- src/client/intl/ja/play/ja.musicselect.ts
+++ src/client/intl/ja/play/ja.musicselect.ts
@@ -1,6 +1,15 @@
 const JAMusicSelectTranslation = {
     "title": "楽曲を選択",
-    "search": "検索",
+    "search": "タイトルを検索",
+    "back": "戻る",
+    "filter": "フィルター",
+    "all": "すべて",
+    "official": "公式楽曲",
+    "fanmade": "拡張/ファンメイド",
+    "details": {
+        "officialMusic": "公式楽曲",
+        "fanmadeMusic": "拡張/ファンメイド楽曲",
+    }
 }
 
 export default JAMusicSelectTranslation;
