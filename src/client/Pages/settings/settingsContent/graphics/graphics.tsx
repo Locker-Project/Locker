1c717530cf8b99360310a49433fe0fe78265da82
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-03-14 20:42:56 +0900

    add : graphics settings

---
+++ src/client/Pages/settings/settingsContent/graphics/graphics.tsx
@@ -0,0 +1,61 @@
+import * as solid from "solid-js";
+import { createStore } from "solid-js/store";
+import { useTransContext } from "@mbarzda/solid-i18next";
+
+import TranslateText from "Components/TranslateText/translateText";
+import RangeInput from "Components/RangeInput/rangeInput";
+
+import SettingsTemplate from "Pages/settings/settingsTemplate/settingsTemplate";
+
+import { getGameConfig } from "Utils/getConfig/getConfig";
+
+import { gameConfigStore, setGameConfigStore } from "State/gameCondigStore";
+
+import { settingsData } from "Types/Settings/settings";
+
+import style from "./graphics.module.scss";
+import ToggleSwitch from "Components/toggleSwitch/toggleSwitch";
+
+
+
+const GraphicsSettings: solid.Component = () => {
+    const [t, intl] = useTransContext();
+    const [graphicsConfig, setGraphicsConfig] = createStore(gameConfigStore.ready ? gameConfigStore.data.graphics : getGameConfig().graphics);
+
+    const graphicsContents: Array<settingsData> = [
+        {
+            label: <TranslateText key="settings.graphics.gameResolution.name" />,
+            input: <RangeInput value={graphicsConfig.resolution} max={200} min={1} onChange={v => setGraphicsConfig("resolution", v)} />,
+            details: {
+                description: <TranslateText key="settings.graphics.gameResolution.description" />
+            }
+        },
+        {
+            label: <TranslateText key="settings.graphics.gameFps.name" />,
+            input: <RangeInput value={graphicsConfig.fps} max={900} min={1} onChange={v => setGraphicsConfig("fps", v)} />,
+            details: {
+                description: <TranslateText key="settings.graphics.gameFps.description" />
+            }
+        },
+        {
+            label: <TranslateText key="settings.graphics.gameAntiAliasing.name" />,
+            input: <ToggleSwitch checked={graphicsConfig.antiAlias} onChange={v => setGraphicsConfig("antiAlias", v.currentTarget.checked)} />,
+            details: {
+                description: <TranslateText key="settings.graphics.gameAntiAliasing.description" />
+            }
+        },
+    ]
+
+    solid.createEffect(() => {
+        setGameConfigStore("data", { graphics: graphicsConfig });
+    });
+
+    return (
+        <SettingsTemplate
+            title={t("settings.graphics.title")}
+            content={graphicsContents}
+        />
+    )
+}
+
+export default GraphicsSettings;
